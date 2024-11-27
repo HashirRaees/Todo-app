537539ec528f1cd13248dd3e033ded45994acc02
HashirRaees <163395402+HashirRaees@users.noreply.github.com>
2024-11-27 19:43:45 +0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,17 +21,17 @@ export default function Page() {
     settodolist(todolist.filter((_, i) => i !== index)); // Filter out the todo by index
   };
   return (
-    <section className="bg-black min-h-screen">
+    <section className="bg-black min-h-svh">
       <div className="bg-purple-500 h-20 flex items-center justify-center flex-row">
-        <h1 className="text-white font-bold text-4xl cursor-default">
+        <h1 className="text-white font-bold sm:text-4xl sm:font-bold text-2xl cursor-default">
           Hashir Raees
         </h1>
-        <span className="text-red-800 text-2xl pl-3 font-bold cursor-default">
+        <span className="text-red-800 sm:text-2xl sm:font-semibold text-lg pl-3 font-bold cursor-default">
           Todo App
-        </span>{" "}
+        </span>
       </div>
 
-      <div className="mt-20 bg-black flex items-center justify-center min-h-80 flex-col">
+      <div className="sm:mt-20 bg-black flex items-center justify-center min-h-80 flex-col">
         <input
           type="text"
           placeholder="Enter task"
@@ -39,11 +39,11 @@ export default function Page() {
           id="todo"
           value={todoName}
           onChange={(e) => handletodoName(e)}
-          className="block leading-10 rounded-xl w-96 px-4 py-2" 
+          className="block leading-10 rounded-xl sm:w-96  px-4 py-2" 
         />
         <div className="space-y-5 flex items-center flex-col">
           <button
-            className="mt-5 bg-purple-600 px-5 py-3 text-white font-bold rounded-2xl text-2xl hover:bg-purple-800 hover:border-2 hover:border-white"
+            className="mt-5 bg-purple-600 px-5 py-3 text-white font-bold rounded-2xl sm:text-2xl hover:bg-purple-800 hover:border-2 hover:border-white"
             onClick={handleAddtodo}
           >
             Add Task
@@ -52,15 +52,15 @@ export default function Page() {
         <ul>
           {todolist.map((todo, index) => (
             <li
-              className="mt-8 cursor-default border-2 flex justify-between items-center opacity-80 border-purple-500 uppercase bg-gray-800 h-14 text-white text-xl px-4 py-2 rounded-2xl w-[400px]"
+              className="mt-5 font-semibold cursor-default border-2 flex justify-between items-center opacity-80 border-purple-500 capitalize bg-gray-800 sm:h-14 text-white sm:text-xl px-4 py-2 rounded-xl sm:w-[400px] w-[200px]"
               key={index}
             >
               {todo}
               <button
-                className="cursor-pointer bg-red-500 rounded-lg h-8 w-8 text-white flex justify-center items-center"
+                className="cursor-pointer bg-red-500 rounded-lg sm:h-8 sm:w-8 w-6 h-6 text-white flex justify-center items-center"
                 onClick={() => handleDeletelist(index)}
               >
-              <RiDeleteBinLine size={20} color="white" className="" />
+              <RiDeleteBinLine size={20} color="white" className="sm:h-20 h-10" />
               </button>
             </li>
           ))}
